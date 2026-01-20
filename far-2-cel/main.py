de9d99f82b6c96a67266a79320a6f8ab3951c0a8
agorbach <agorbach@gmail.com>
2026-01-20 19:26:15 +0200

    Update main.py

--- far-2-cel/main.py
+++ far-2-cel/main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
+import socket
 
 app = Flask(__name__)
 
@@ -12,14 +12,32 @@ def index():
         fahrenheit = ""
     return (
         """<form action="" method="get" style="text-align: center;">
-                <div style="background-color: green; color: black; font-size: 2.7em; display: unset; padding: 5px;">
-                Celsius temperature: <input type="text" name="celsius" value=""" + celsius + """>
+                <div style="background-color: green; color: black; font-size: 2.7em; display: unset; padding: 10px;">
+                Celsius temperature: 
+                <input type="text" name="celsius" value=\"""" + celsius + """\">
                 <input type="submit" value="Convert to Fahrenheit">
             </div>
             """
-        + """<br><br><div style="background-color: lime;color: salmon; font-size: 2.6em; display: unset; padding: 6px;">Fahrenheit: <b>"""
-        + fahrenheit + "</b></div></form>"
+        + """<br><br>
+            <div style="background-color: lime; color: salmon; font-size: 2.6em; display: unset; padding: 10px;">
+                Fahrenheit: <b>""" + fahrenheit + """</b>
+            </div>
+        </form>"""
     )
+
+
+@app.route("/whoami")
+def whoami():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return f"""
+    <div style="text-align: center; font-size: 2em; margin-top: 40px;">
+        <b>Who answered this request?</b><br><br>
+        Pod name: <span style="color: blue;">{hostname}</span><br>
+        Pod IP: <span style="color: green;">{ip}</span>
+    </div>
+    """
+
 
 def fahrenheit_from(celsius):
     """Convert Celsius to Fahrenheit degrees."""
@@ -29,6 +47,7 @@ def fahrenheit_from(celsius):
         return str(fahrenheit)
     except ValueError:
         return "invalid input"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
